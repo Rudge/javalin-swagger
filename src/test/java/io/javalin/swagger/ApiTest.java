61104bbc800b850ee6c55987451864333e079ebb
Andrei Shikov <andreiishikov@gmail.com>
2018-05-03 22:14:54 +0100

    Add Java example

---
+++ src/test/java/io/javalin/swagger/ApiTest.java
@@ -0,0 +1,34 @@
+package io.javalin.swagger;
+
+import io.javalin.Javalin;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.parameters.Parameter;
+
+import static io.javalin.swagger.DocumentedHandler.documented;
+import static io.javalin.swagger.JavalinExtKt.serveSwagger;
+import static io.javalin.swagger.SwaggerDefKt.route;
+
+public class ApiTest {
+
+    public static void main(String... args) {
+
+        OpenAPI api = new OpenAPI()
+            .info(new Info().title("Test"));
+
+        Javalin app = Javalin.create()
+            .port(8080)
+            .enableCorsForAllOrigins()
+            .post("test/:id", documented(route()
+                .addParametersItem(
+                    new Parameter()
+                        .name("id")
+                        .in("path")
+                        .allowEmptyValue(true)), ctx -> {
+
+            }))
+            .get("test", documented(route(), ctx -> { }));
+
+        serveSwagger(app, api).start();
+    }
+}
